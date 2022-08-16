478036f25e5d380c592bcc875fb4efae979ec309
Longs Pemun Gotar <developerlongs@gmail.com>
2022-08-16 21:44:09 +0000

    deployment scripts

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,29 @@
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
+// require("@nomiclabs/hardhat-waffle");
+import "@nomiclabs/hardhat-ethers";
+require("@nomiclabs/hardhat-etherscan");
+require("dotenv").config({ path: ".env" });
 
-const config: HardhatUserConfig = {
+const ALCHEMY_GOERLI_API_KEY_URL = process.env.ALCHEMY_GOERLI_API_KEY_URL;
+
+const ACCOUNT_PRIVATE_KEY = process.env.ACCOUNT_PRIVATE_KEY;
+
+const API_TOKEN = process.env.API_TOKEN;
+
+module.exports = {
   solidity: "0.8.9",
-};
+  networks: {
+    hardhat: {
+      forking: {
+        url: ALCHEMY_GOERLI_API_KEY_URL,
+      },
+    },
 
-export default config;
+    goerli: {
+      url: ALCHEMY_GOERLI_API_KEY_URL,
+      accounts: [ACCOUNT_PRIVATE_KEY],
+    },
+  },
+  etherscan: {
+    apiKey: API_TOKEN,
+  },
+};
